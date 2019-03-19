01f207374f07f1205d673067ddba564d495ea200
Enrique Rodríguez Castañeda <zerh0gie@hotmail.com>
2019-03-19 13:20:03 +0100

    Historial:
    Elementos en la lista,
    Botón de acceso

--- app/src/main/java/es/udc/fic/muei/atopate/HistorialActivity.java
+++ app/src/main/java/es/udc/fic/muei/atopate/HistorialActivity.java
@@ -1,13 +1,46 @@
 package es.udc.fic.muei.atopate;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class HistorialActivity extends Activity {
+
+    ArrayList<ItemHistorialActivity> historials = new ArrayList<ItemHistorialActivity>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial);
+
+        ListView listV = (ListView) findViewById(R.id.historial_list);
+
+        Drawable icono = Drawable.createFromPath("@drawable/ic_launcher_background.xml");
+
+        //TODO enlazar a la base de datos y alimentar desde ahí
+        historials.add(new ItemHistorialActivity("Hace 3 horas", "A coruña", "Madrid", "530km", icono));
+        historials.add(new ItemHistorialActivity("Lunes, 25/02/2019", "Av. dos Mallos", "Pza Pontevedra", "2km", icono));
+        for (int i = 0; i<10; i++) {
+            historials.add(new ItemHistorialActivity());
+        }
+
+        AdapterItem adapter = new AdapterItem(this, historials);
+
+        listV.setAdapter(adapter);
+
+        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final int pos = position;
+                Toast.makeText(getApplicationContext(), "Apretado " + Integer.toString(position),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
--- app/src/main/java/es/udc/fic/muei/atopate/MainActivity.java
+++ app/src/main/java/es/udc/fic/muei/atopate/MainActivity.java
@@ -1,13 +1,29 @@
 package es.udc.fic.muei.atopate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button historialButton = (Button) findViewById(R.id.HistoriaButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        historialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        MainActivity.this,
+                        HistorialActivity.class));
+            }
+        });
     }
+
+
 }
