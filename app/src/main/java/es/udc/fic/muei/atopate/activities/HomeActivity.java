20d80eb2deb77451d5d06a3b96bf4afce7b3c5b0
costris95 <costris12@gmail.com>
2019-03-27 19:39:01 +0100

    Tarea 6 - Fragment Inicio (Revert merge Ramas Elisa)

--- app/src/main/java/es/udc/fic/muei/atopate/activities/HomeActivity.java
+++ app/src/main/java/es/udc/fic/muei/atopate/activities/HomeActivity.java
@@ -1,44 +1,16 @@
 package es.udc.fic.muei.atopate.activities;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import es.udc.fic.muei.atopate.AjustesActivity;
-import es.udc.fic.muei.atopate.EstadisticasActivity;
-import es.udc.fic.muei.atopate.HistorialActivity;
 import es.udc.fic.muei.atopate.R;
-import es.udc.fic.muei.atopate.TrayectoActivity;
-import es.udc.fic.muei.atopate.fragments.AjustesFragment;
-import es.udc.fic.muei.atopate.fragments.EstadisticasFragment;
 import es.udc.fic.muei.atopate.fragments.HomeFragment;
 import es.udc.fic.muei.atopate.fragments.TrayectoFragment;
-import es.udc.fic.muei.atopate.maps.MapsConfigurer;
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.PieChartView;
 
 public class HomeActivity extends AppCompatActivity  {
 
@@ -71,7 +43,7 @@ public class HomeActivity extends AppCompatActivity  {
 
                 case R.id.navigation_resumen:
 
-                    fragmentToSubstitute = EstadisticasFragment.newInstance();
+                    fragmentToSubstitute = HomeFragment.newInstance();
 
                     validNavigationItemSelected = true;
                     break;
@@ -85,7 +57,7 @@ public class HomeActivity extends AppCompatActivity  {
 
                 case R.id.navigation_ajustes:
 
-                    fragmentToSubstitute = AjustesFragment.newInstance();
+                    fragmentToSubstitute = HomeFragment.newInstance();
 
                     validNavigationItemSelected = true;
                     break;
