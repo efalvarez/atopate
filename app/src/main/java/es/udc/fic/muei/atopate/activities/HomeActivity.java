7d99a631fc11129dab53fc0906d1cf43d21b5799
Enrique Rodríguez Castañeda <zerh0gie@hotmail.com>
2019-03-28 12:10:38 +0100

    Tarea6, Integración:
    Eliminando dependencias sin utilizar.

--- app/src/main/java/es/udc/fic/muei/atopate/activities/HomeActivity.java
+++ app/src/main/java/es/udc/fic/muei/atopate/activities/HomeActivity.java
@@ -1,44 +1,17 @@
 package es.udc.fic.muei.atopate.activities;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import es.udc.fic.muei.atopate.AjustesActivity;
-import es.udc.fic.muei.atopate.EstadisticasActivity;
-import es.udc.fic.muei.atopate.HistorialActivity;
 import es.udc.fic.muei.atopate.R;
-import es.udc.fic.muei.atopate.TrayectoActivity;
 import es.udc.fic.muei.atopate.fragments.HistorialFragment;
 import es.udc.fic.muei.atopate.fragments.HomeFragment;
 import es.udc.fic.muei.atopate.fragments.TrayectoFragment;
-import es.udc.fic.muei.atopate.maps.MapsConfigurer;
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.PieChartView;
 
 public class HomeActivity extends AppCompatActivity {
 
