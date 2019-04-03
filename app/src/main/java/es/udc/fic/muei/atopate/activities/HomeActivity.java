2adfac7ba6ea26d79ea705f329f839a93416731b
costris95 <costris12@gmail.com>
2019-04-03 18:11:13 +0200

    Tarea 7 - Multimedia costa, prueba

--- app/src/main/java/es/udc/fic/muei/atopate/activities/HomeActivity.java
+++ app/src/main/java/es/udc/fic/muei/atopate/activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package es.udc.fic.muei.atopate.activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,17 +13,17 @@ import android.view.View;
 import android.widget.Toast;
 
 import es.udc.fic.muei.atopate.R;
+import es.udc.fic.muei.atopate.fragments.AjustesFragment;
+import es.udc.fic.muei.atopate.fragments.EstadisticasFragment;
 import es.udc.fic.muei.atopate.fragments.HistorialFragment;
 import es.udc.fic.muei.atopate.fragments.HomeFragment;
 import es.udc.fic.muei.atopate.fragments.TrayectoFragment;
-import es.udc.fic.muei.atopate.fragments.AjustesFragment;
-import es.udc.fic.muei.atopate.fragments.EstadisticasFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String TAG = HomeActivity.class.getSimpleName();
-
     private BottomNavigationView bottomNavigationView;
+    private String pathFile;
+    private Uri capturedImageURI;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -95,7 +96,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         configureBottomNavigation();
     }
 
@@ -129,7 +129,6 @@ public class HomeActivity extends AppCompatActivity {
     private void configureBottomNavigation() {
         bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
     }
 
     // HOME FRAGMENT CLICK LISTENERS
@@ -141,4 +140,87 @@ public class HomeActivity extends AppCompatActivity {
         Toast.makeText(view.getContext(), "Compartir", Toast.LENGTH_SHORT).show();
     }
 
+    public void setCapturedImageURI(Uri fileUri) {
+        capturedImageURI = fileUri;
+    }
+
+    public void setCurrentPhotoPath(String path) {
+        pathFile = path;
+    }
+
+    public Uri getCapturedImageURI() {
+        return capturedImageURI;
+    }
+
+    public String getCurrentPhotoPath() {
+        return pathFile;
+    }
+
+  /*  private final static String CAPTURED_PHOTO_PATH_KEY = "mCurrentPath";
+    private final static String CAPTURED_PHOTO_URI_KEY = "mCapturedImageURI";
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        if (pathFile != null) {
+            savedInstanceState.putString(CAPTURED_PHOTO_PATH_KEY, pathFile);
+        }
+        if (capturedImageURI != null) {
+            savedInstanceState.putString(CAPTURED_PHOTO_URI_KEY, capturedImageURI.toString());
+        }
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(CAPTURED_PHOTO_PATH_KEY)) {
+            pathFile = savedInstanceState.getString(CAPTURED_PHOTO_PATH_KEY);
+        }
+        if (savedInstanceState.containsKey(CAPTURED_PHOTO_URI_KEY)) {
+            capturedImageURI = Uri.parse(savedInstanceState.getString(CAPTURED_PHOTO_URI_KEY));
+        }
+        super.onRestoreInstanceState(savedInstanceState);
+    } */
+
+    /* static final int REQUEST_PICTURE_CAPTURE = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            addToGallery();
+            ImageView image = findViewById(R.id.imageView);
+            setPic(getCurrentPhotoPath(), image);
+        }
+
+    private void setPic(String imagePath, ImageView imageView) {
+        int targetW = imageView.getWidth(); // Get the dimensions of the View
+        int targetH = imageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth; // Get the dimensions of the bitmap
+        int photoH = bmOptions.outHeight;
+// Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+// Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private void addToGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(this.getCurrentPhotoPath());
+        Uri picUri = Uri.fromFile(f);
+        galleryIntent.setData(picUri);
+        this.sendBroadcast(galleryIntent);
+    } */
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    } */
 }
--- app/src/main/java/es/udc/fic/muei/atopate/fragments/HomeFragment.java
+++ app/src/main/java/es/udc/fic/muei/atopate/fragments/HomeFragment.java
@@ -1,12 +1,24 @@
 package es.udc.fic.muei.atopate.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.MapView;
 import com.jjoe64.graphview.GraphView;
@@ -14,7 +26,11 @@ import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import es.udc.fic.muei.atopate.R;
@@ -24,10 +40,20 @@ import lecho.lib.hellocharts.model.PieChartData;
 import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.view.PieChartView;
 
+import static android.app.Activity.RESULT_OK;
+
 public class HomeFragment extends Fragment {
 
 
     MapView mapaVista;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private ImageView image;
+
+    private String pictureFilePath;
+    private String deviceIdentifier;
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -51,8 +77,18 @@ public class HomeFragment extends Fragment {
 
         View viewinflated = inflater.inflate(R.layout.fragment_home, container, false);
 
+        image = viewinflated.findViewById(R.id.imageView);
+
+        Button captureButton = viewinflated.findViewById(R.id.photo);
+        captureButton.setOnClickListener(capture);
+        if (!getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) && captureButton != null) {
+            captureButton.setEnabled(false);
+        }
+
         configureCharts(viewinflated);
         configureMaps(viewinflated, savedInstanceState);
+
+        // getInstallationIdentifier();
 
         return viewinflated;
     }
@@ -149,4 +185,109 @@ public class HomeFragment extends Fragment {
         pieChartView.setPieChartData(pieChartData);
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+// Ensure that there's a camera activity to handle the intent
+        HomeActivity activity = (HomeActivity) getActivity();
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile = null;
+            try { // Create the File where the photo should go
+                photoFile = createImageFile();
+            } catch (IOException ex) {// Error occurred while creating the File
+                Toast.makeText(this.getContext(),
+                        "Photo file can't be created, please try again",
+                        Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "EXCEPCION: " + ex.toString());
+                return;
+            }
+            if (photoFile != null) {
+                //Uri fileUri = Uri.fromFile(photoFile);
+                Uri fileUri = FileProvider.getUriForFile(getContext(),
+                        getContext().getApplicationContext().getPackageName() + ".provider", photoFile);
+                activity.setCapturedImageURI(fileUri);
+                //activity.setCurrentPhotoPath(pictureFilePath);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        activity.getCapturedImageURI());
+                activity.setResult(RESULT_OK);
+                HomeFragment homeFragment = this;
+                homeFragment.startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+// Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new
+                Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        // File outputDir = getContext().getCacheDir();
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        Log.d(TAG, "valor de imageFileName: " + imageFileName);
+        Log.d(TAG, "valor de storageDir: " + storageDir);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        pictureFilePath = image.getAbsolutePath();
+        HomeActivity activity = (HomeActivity) getActivity();
+        //activity.setCurrentPhotoPath("file:" + pictureFilePath);
+        activity.setCurrentPhotoPath("file://"+ pictureFilePath);
+        Log.d(TAG, "valor de picture: " + pictureFilePath);
+
+        return image;
+    }
+
+    private View.OnClickListener capture = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                dispatchTakePictureIntent();
+            }
+        }
+    };
+
+    private void addToGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        HomeActivity activity = (HomeActivity) getActivity();
+        File f = new File(activity.getCurrentPhotoPath());
+        Uri picUri = Uri.fromFile(f);
+        galleryIntent.setData(picUri);
+        this.getActivity().sendBroadcast(galleryIntent);
+    }
+
+    private void setPic(String imagePath, ImageView imageView) {
+        int targetW = imageView.getWidth(); // Get the dimensions of the View
+        int targetH = imageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth; // Get the dimensions of the bitmap
+        int photoH = bmOptions.outHeight;
+// Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+// Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    static final int REQUEST_PICTURE_CAPTURE = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+       // super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            HomeActivity activity = (HomeActivity) getActivity();
+            addToGallery();
+            setPic(activity.getCurrentPhotoPath(), image);
+        }
+    }
 }
