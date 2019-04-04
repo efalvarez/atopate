21cec1092ff8c4335749d01e87e54e930710ee86
costris95 <costris12@gmail.com>
2019-04-04 17:01:52 +0200

    Tarea 7 - Integracion

--- app/src/main/java/es/udc/fic/muei/atopate/fragments/HomeFragment.java
+++ app/src/main/java/es/udc/fic/muei/atopate/fragments/HomeFragment.java
@@ -96,6 +96,7 @@ public class HomeFragment extends Fragment {
         }
 
         if (activity.getBipMap() != null && image != null) {
+
             image.setImageBitmap(activity.getBipMap());
             image.setOnClickListener(new View.OnClickListener() {
                 @Override
